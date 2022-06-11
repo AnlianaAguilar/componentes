1a65f0cfdf6cb50a3a1c53f38efc8aee0373f5d0
anlianaaguilar <anlianaaguilar@gmail.com>
2022-06-10 22:12:37 -0400

    se crea character

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      character:[
+        {
+          nombre: "Rick Sanchez",
+          src:"https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+        },
+        {
+          nombre: "Morty Smith",
+          src:"https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        },
+        {
+          nombre: "Summer Smith",
+          src:"https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+        },
+        {
+          nombre: "Beth Smith",
+          src:"https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+        },
+        {
+          nombre: "Jerry Smith",
+          src:"https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+        },
+        {
+          nombre: "Abadango Cluster Princess",
+          src:"https://rickandmortyapi.com/api/character/avatar/6.jpeg"
+        },
+        {
+          nombre: "Abradolf Lincler",
+          src:"https://rickandmortyapi.com/api/character/avatar/7.jpeg"
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
